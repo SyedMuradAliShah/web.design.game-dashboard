e183df419e870978f1036d82f50fdef0d163aa98
sulaiman-khan <sallokhan01999@gmail.com>
2025-06-26 17:21:08 +0500

    commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,274 @@
+// ================================password
+$(document).ready(function() {
+    const passwordInput = $("#password");
+    const toggleButton = $("#togglePassword");
+
+    toggleButton.on("click", function() {
+        if (passwordInput.attr("type") === "password") {
+            passwordInput.attr("type", "text");
+            toggleButton.removeClass("far fa-eye").addClass("far fa-eye-slash");
+        } else {
+            passwordInput.attr("type", "password");
+            toggleButton.removeClass("far fa-eye-slash").addClass("far fa-eye");
+        }
+    });
+});
+
+// ================================password
+
+
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('.navigation');
+  const scrollY = window.scrollY;
+
+  // Adjust this value to set the scroll threshold
+  const scrollThreshold = 100;
+
+  if (scrollY > scrollThreshold) {
+      header.classList.add('sticky');
+  } else {
+      header.classList.remove('sticky');
+  }
+});
+
+
+// ================================slider-1
+
+$('#myNft').owlCarousel({
+  stagePadding: 100,
+  loop:true,
+  margin:30,
+  nav:false,
+  dots:false,
+  responsive:{
+      0:{
+        stagePadding: 0,
+          items:1,
+          dots:true,
+      },
+      600:{
+          items:2,
+          dots:true,
+      },
+      1000:{
+          items:3,
+          dots:true,
+      },
+      1280:{
+          items:4,
+          dots:true,
+      }
+  }
+})
+// ================================slider-1
+// ================================slider-2
+$('#myPartner').owlCarousel({
+  loop:true,
+  margin:10,
+  // nav:true,
+  dots:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+// ================================slider-2
+// ================================slider-3
+$('#myServices').owlCarousel({
+  loop:true,
+  margin:30,
+  // nav:true,
+  dots:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+// ================================slider-3
+// ================================slider-4
+$('#myTrading').owlCarousel({
+  loop:true,
+  margin:20,
+  // nav:true,
+  dots:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+// ================================slider-4
+// ================================slider-5
+$('#heroSec').owlCarousel({
+  loop:true,
+  margin:10,
+  // nav:true,
+  dots:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+  }
+})
+// ================================slider-5
+// ================================slider-6
+$('#myTestmonial').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  dots:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:2
+      }
+  }
+})
+
+// ================================slider-6
+// var swiper = new Swiper(".mySwiper", {
+//   loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   autoplay: {
+//     delay: 3555000,
+//     disableOnInteraction: false,
+//   },
+// });
+
+// // ============slider-2
+// var swiper = new Swiper(".myTrading", {
+//   slidesPerView: 5,
+//   spaceBetween: 30,
+//   loop: true,
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//     },
+//     768: {
+//       slidesPerView: 3,
+//       spaceBetween: 40,
+//     },
+//     1024: {
+//       slidesPerView: 5,
+//       spaceBetween: 50,
+//     },
+//   },
+// });
+// // ============slider-2
+
+// // ============slider-3
+// var swiper = new Swiper(".myServices", {
+//   loop: true,
+//   slidesPerView: 4,
+//   spaceBetween: 40,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   autoplay: {
+//     delay: 3500,
+//     disableOnInteraction: false,
+//   },
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//     },
+//     1024: {
+//       slidesPerView: 4,
+//     },
+//   },
+// });
+// // ============slider-3
+// // ============slider-4
+// var swiper = new Swiper(".myTestmonial", {
+//   loop: true,
+//   slidesPerView: 2,
+//   spaceBetween: 30,
+//   autoplay: {
+//     delay: 3500,
+//     disableOnInteraction: false,
+//   },
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 20,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 40,
+//     },
+//     1024: {
+//       slidesPerView: 2,
+//       spaceBetween: 50,
+//     },
+//   },
+// });
+// // ============slider-4
+// // ============slider-5
+// var swiper = new Swiper(".myPartner", {
+//   slidesPerView: 5,
+//   spaceBetween: 30,
+//   loop: true,
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//     },
+//     768: {
+//       slidesPerView: 3,
+//       spaceBetween: 40,
+//     },
+//     1024: {
+//       slidesPerView: 5,
+//       spaceBetween: 50,
+//     },
+//   },
+// });
+// // ============slider-5
+
---
+++ gamedashboard/assets/js/javascript.js
@@ -0,0 +1,88 @@
+//  ============================ image-preview
+// script.js
+var btnUpload = $("#upload_file"),
+		btnOuter = $(".button_outer");
+	btnUpload.on("change", function(e){
+		var ext = btnUpload.val().split('.').pop().toLowerCase();
+		if($.inArray(ext, ['gif','png','jpg','jpeg']) == -1) {
+			$(".error_msg").text("Not an Image...");
+		} else {
+			$(".error_msg").text("");
+			btnOuter.addClass("file_uploading");
+			setTimeout(function(){
+				btnOuter.addClass("file_uploaded");
+			},3000);
+			var uploadedFile = URL.createObjectURL(e.target.files[0]);
+			setTimeout(function(){
+				$("#uploaded_view").append('<img src="'+uploadedFile+'" />').addClass("show");
+			},3500);
+		}
+	});
+	$(".file_remove").on("click", function(e){
+		$("#uploaded_view").removeClass("show");
+		$("#uploaded_view").find("img").remove();
+		btnOuter.removeClass("file_uploading");
+		btnOuter.removeClass("file_uploaded");
+	});
+
+
+
+
+  const videoContainers = document.querySelectorAll('.video-iframe');
+  const skipButton = document.getElementById('skip-button');
+  const videoNumber = document.getElementById('video-number');
+  let currentVideoIndex = 0;
+  let timer;
+
+  function playCurrentVideo() {
+      videoContainers[currentVideoIndex].style.display = 'block';
+      updateVideoNumber();
+      startSkipTimer(5); // Start the timer with 5 seconds
+  }
+
+  function hideAllVideos() {
+      videoContainers.forEach(video => {
+          video.style.display = 'none';
+      });
+  }
+
+  function skipToNextVideo() {
+      hideAllVideos();
+      currentVideoIndex = (currentVideoIndex + 1) % videoContainers.length;
+      playCurrentVideo();
+  }
+
+  function updateVideoNumber() {
+      videoNumber.textContent = `${currentVideoIndex + 1}`;
+  }
+
+  function startSkipTimer(seconds) {
+      let remainingTime = seconds;
+      skipButton.disabled = true;
+      skipButton.style.cursor = 'not-allowed';
+
+      function updateButtonLabel() {
+          if (remainingTime === 0) {
+              clearInterval(timer);
+              skipButton.textContent = 'Watch Next';
+              skipButton.disabled = false;
+              skipButton.style.cursor = 'pointer';
+          } else {
+              skipButton.textContent = `Skip (${remainingTime})`;
+          }
+          remainingTime--;
+      }
+
+      updateButtonLabel();
+      timer = setInterval(updateButtonLabel, 1000); // Update every second (1000 milliseconds)
+  }
+
+  // Play the first video
+  playCurrentVideo();
+
+  // Add a click event listener to the skip button
+  skipButton.addEventListener('click', skipToNextVideo);
+
+
+
+
